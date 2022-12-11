5b5c19c256ce54397836b2ccc39befb31506bf3f
yeon <ohy9205@naver.com>
2022-12-12 00:25:28 +0900

    2.1 login form 생성

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Auth() {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeHandler = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={onChangeHandler}
+          placeholder="Emial"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={onChangeHandler}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      <div>
+        <button>continue with Google</button>
+        <button>continue with GitHub</button>
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
