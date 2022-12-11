b1040528b0b95a58f9b061f017c0f479eee60c71
yeon <ohy9205@naver.com>
2022-12-11 20:15:09 +0900

    1.0 firebase셋업

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import firebase from "./firebase";
+
 function App() {
   return <div>App</div>;
 }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZTdlfd-ne_mvB4OKkwaFy1549et12AE4",
+  authDomain: "nwitter-719fe.firebaseapp.com",
+  projectId: "nwitter-719fe",
+  storageBucket: "nwitter-719fe.appspot.com",
+  messagingSenderId: "990557878619",
+  appId: "1:990557878619:web:b84686edac6688fcb8427e",
+};
+
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
