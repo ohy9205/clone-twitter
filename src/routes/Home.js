fd1bbdb4879b18425e61bdeda3286b7e1f85892c
yeon <ohy9205@naver.com>
2022-12-12 15:25:38 +0900

    3.2 nweet목록 조회 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
-import { collection, addDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import { dbService } from "../firebase";
 
 function Home() {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  //db로부터 nweets를 불러오는 메소드
+  const getNweets = async () => {
+    const querySnapshot = await getDocs(collection(dbService, "nweets"));
+    querySnapshot.forEach((doc) => {
+      const nweetObj = { id: doc.id, ...doc.data() };
+      setNweets((prev) => [nweetObj, ...prev]);
+    });
+    console.log(nweets);
+  };
+
+  //컴포넌트가 마운트되면 db에 저장된 nweets들을 불러온다
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
 
-    const collectionRef = await addDoc(collection(dbService, "nweets"), {
+    const docRef = await addDoc(collection(dbService, "nweets"), {
       nweet,
       createdAt: Date.now(),
     });
@@ -34,6 +50,13 @@ function Home() {
         />
         <button type="submit">Nweet</button>
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
