0452a7b26d09ce857f5e34b2b1317dd82caacaa4
yeon <ohy9205@naver.com>
2022-12-12 14:17:40 +0900

    3.0 nweet 입력폼 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const [nweet, setNweet] = useState("");
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const onChangeHandler = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={100}
+          value={nweet}
+          onChange={onChangeHandler}
+        />
+        <button type="submit">Nweet</button>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
