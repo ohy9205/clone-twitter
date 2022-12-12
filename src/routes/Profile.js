184fc1e51e00e27919e14a5028c0f4cceb518dc3
yeon <ohy9205@naver.com>
2022-12-12 13:59:16 +0900

    2.6 로그아웃 후 redirect처리

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { authService } from "../firebase";
 
 function Profile() {
-  return <div>Profile</div>;
+  const navigate = useNavigate();
+
+  // 로그아웃 처리
+  const onLogoutHandler = () => {
+    authService.signOut();
+
+    //로그아웃되면 Home으로 돌아가야한다.
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <button onClick={onLogoutHandler}>Logout</button>
+    </div>
+  );
 }
 
 export default Profile;
