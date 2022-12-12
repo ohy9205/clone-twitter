a3872900707c73836207a17a70f8a0dffe99930e
yeon <ohy9205@naver.com>
2022-12-12 13:41:35 +0900

    2.6 home, profile 페이지 링크 처리

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 
 function AppRouter({ isLoggedIn }) {
   const showHome = <Route path="/" exact element={<Home />} />;
@@ -10,9 +12,10 @@ function AppRouter({ isLoggedIn }) {
   return (
     <div>
       <BrowserRouter>
+        {isLoggedIn && <Navigation />}
         <Routes>
           {isLoggedIn ? showHome : loginPage}
-          <Route path="" element={""} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
