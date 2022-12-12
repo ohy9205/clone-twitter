beb04a2ff8906c8ae92182637d1960a0121bf917
yeon <ohy9205@naver.com>
2022-12-12 20:36:02 +0900

    3.5 삭제 기능 구현

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { doc, deleteDoc } from "firebase/firestore";
+import { dbService } from "../firebase";
 
 function Nweet({ nweetObj, isOwner }) {
+  //글 삭제
+  const onDeleteHandler = async () => {
+    const ok = window.confirm("삭제하시겠습니까?");
+    if (ok) {
+      // delete 글
+      await deleteDoc(doc(dbService, "nweets", nweetObj.id));
+    } else {
+      // delete 취소
+      return;
+    }
+  };
+
   return (
     <div>
       <h4>{nweetObj.text}</h4>
       {isOwner && (
         <>
-          <button>Delete Nweet</button>
+          <button onClick={onDeleteHandler}>Delete Nweet</button>
           <button>Edit Nweet</button>
         </>
       )}
