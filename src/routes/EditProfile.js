c5c2cc29ece22a8ddc1848ab4f13e4ef85bc330d
yeon <ohy9205@naver.com>
2022-12-13 18:19:22 +0900

    5.0 profile에서 정보 받아오기

--- src/routes/EditProfile.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function EditProfile() {
-  return <div>Edit profile</div>;
-}
-
-export default EditProfile;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { authService } from "../firebase";
+import {
+  collection,
+  where,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import { authService, dbService } from "../firebase";
 
-function Profile() {
+function Profile({ userInfo }) {
   const navigate = useNavigate();
 
   // 로그아웃 처리
@@ -11,6 +19,25 @@ function Profile() {
     //로그아웃되면 Home으로 돌아가야한다.
     navigate("/");
   };
+
+  // 프로파일 페이지에 접근한 유저 정보를 불러온다
+  useEffect(() => {
+    const getMyNweets = () => {
+      //db 컬렉션 생성
+      const nweetsCollection = collection(dbService, "nweets");
+      //사용자 정보 뽑아내는 필터링 쿼리 작성
+      const q = query(
+        nweetsCollection,
+        where("authorId", "==", userInfo.uid),
+        orderBy("createdAt", "asc")
+      );
+      //쿼리문으로 정보 불러옴
+      const nweets = onSnapshot(q, (querySnapshot) => {
+        querySnapshot.docs.map((doc) => console.log(doc.data()));
+      });
+    };
+    getMyNweets();
+  }, []);
 
   return (
     <div>
