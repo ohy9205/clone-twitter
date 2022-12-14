781d26b23ad7526a58b10e2d1040a22114d6929b
yeon <ohy9205@naver.com>
2022-12-14 10:24:52 +0900

    6.0 NweetFactory생성해서 Nweet작성 form 분리

---
+++ src/components/NweetFactory.js
@@ -0,0 +1,105 @@
+import React, { useRef, useState } from "react";
+import { dbService, storageService } from "../firebase";
+import { ref, uploadString, getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
+import { collection, addDoc } from "firebase/firestore";
+
+function NweetFactory({ userInfo }) {
+  const [nweetText, setNweetText] = useState("");
+  const [attachment, setAttachment] = useState(""); // data:image/형식의 file string
+  const fileInput = useRef();
+  const nweetsCollection = collection(dbService, "nweets");
+
+  const onChangeHandler = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweetText(value);
+  };
+
+  //첨부파일 처리
+  const onFileChange = (e) => {
+    const {
+      target: { files },
+    } = e;
+
+    // 파일 정보를 가져온다
+    const theFile = files[0]; //한개의 파일만 첨부하므로 항상 files[0]
+
+    // 파일 정보를 읽는다
+    const reader = new FileReader();
+    // 파일이 읽기가 끝났을 때 실행
+    reader.onload = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    let attachmentUrl;
+    if (attachment) {
+      // 파일에 접근하는 레퍼런스 생성
+      // 업로드 유저를 구분하기 위해 userInfo.uid 이름의 폴더를 생성한다
+      const attachmentRef = ref(storageService, `${userInfo.uid}/${uuidv4()}`);
+      // 파일 업로드
+      const response = await uploadString(
+        attachmentRef,
+        attachment,
+        "data_url"
+      );
+      // 다운로드 위한 파일 URL 생성
+      attachmentUrl = await getDownloadURL(ref(storageService, attachmentRef));
+    }
+
+    //저장할 nweet객체 생성
+    const nweet = {
+      text: nweetText,
+      createdAt: Date.now(),
+      authorId: userInfo.uid,
+      attachmentUrl: attachment ? attachmentUrl : "",
+    };
+
+    await addDoc(nweetsCollection, nweet);
+    setNweetText("");
+    setAttachment("");
+    onClearAttachment();
+  };
+
+  const onClearAttachment = () => {
+    setAttachment(null);
+    fileInput.current.value = "";
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <input
+        type="text"
+        placeholder="What's on your mind?"
+        maxLength={100}
+        value={nweetText}
+        onChange={onChangeHandler}
+      />
+      <input
+        ref={fileInput}
+        type="file"
+        accept="image/*"
+        onChange={onFileChange}
+      />
+      <br />
+      <button type="submit">Nweet</button>
+      {attachment && (
+        <div>
+          <img src={attachment} width="50px" />
+          <button onClick={onClearAttachment}>Clear</button>
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default NweetFactory;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,87 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
-import {
-  collection,
-  addDoc,
-  onSnapshot,
-  query,
-  orderBy,
-} from "firebase/firestore";
-import { ref, uploadString, getDownloadURL } from "firebase/storage";
-import { v4 as uuidv4 } from "uuid";
-import { dbService, storageService } from "../firebase";
+import React, { useEffect, useState } from "react";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+import { dbService } from "../firebase";
 import Nweet from "../components/Nweet";
+import NweetFactory from "../components/NweetFactory";
 
 function Home({ userInfo }) {
-  const [nweetText, setNweetText] = useState("");
   const [nweets, setNweets] = useState([]);
-  const [attachment, setAttachment] = useState(""); // data:image/형식의 file string
-  const fileInput = useRef();
   const nweetsCollection = collection(dbService, "nweets");
-
-  const onChangeHandler = (e) => {
-    const {
-      target: { value },
-    } = e;
-    setNweetText(value);
-  };
-
-  //첨부파일 처리
-  const onFileChange = (e) => {
-    const {
-      target: { files },
-    } = e;
-
-    // 파일 정보를 가져온다
-    const theFile = files[0]; //한개의 파일만 첨부하므로 항상 files[0]
-
-    // 파일 정보를 읽는다
-    const reader = new FileReader();
-    // 파일이 읽기가 끝났을 때 실행
-    reader.onload = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-
-  const onSubmitHandler = async (e) => {
-    e.preventDefault();
-
-    let attachmentUrl;
-    if (attachment) {
-      // 파일에 접근하는 레퍼런스 생성
-      // 업로드 유저를 구분하기 위해 userInfo.uid 이름의 폴더를 생성한다
-      const attachmentRef = ref(storageService, `${userInfo.uid}/${uuidv4()}`);
-      // 파일 업로드
-      const response = await uploadString(
-        attachmentRef,
-        attachment,
-        "data_url"
-      );
-      // 다운로드 위한 파일 URL 생성
-      attachmentUrl = await getDownloadURL(ref(storageService, attachmentRef));
-    }
-
-    //저장할 nweet객체 생성
-    const nweet = {
-      text: nweetText,
-      createdAt: Date.now(),
-      authorId: userInfo.uid,
-      attachmentUrl: attachment ? attachmentUrl : "",
-    };
-
-    await addDoc(nweetsCollection, nweet);
-    setNweetText("");
-    setAttachment("");
-    onClearAttachment();
-  };
-
-  const onClearAttachment = () => {
-    setAttachment(null);
-    fileInput.current.value = "";
-  };
 
   //컴포넌트가 마운트되면 db에 저장된 nweets들을 불러온다
   useEffect(() => {
@@ -99,29 +24,9 @@ function Home({ userInfo }) {
 
   return (
     <section>
-      <form onSubmit={onSubmitHandler}>
-        <input
-          type="text"
-          placeholder="What's on your mind?"
-          maxLength={100}
-          value={nweetText}
-          onChange={onChangeHandler}
-        />
-        <input
-          ref={fileInput}
-          type="file"
-          accept="image/*"
-          onChange={onFileChange}
-        />
-        <br />
-        <button type="submit">Nweet</button>
-        {attachment && (
-          <div>
-            <img src={attachment} width="50px" />
-            <button onClick={onClearAttachment}>Clear</button>
-          </div>
-        )}
-      </form>
+      <div>
+        <NweetFactory userInfo={userInfo} />
+      </div>
       <div>
         {nweets.map((nweet) => (
           <Nweet
